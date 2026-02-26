a2c8828ce2678d5ea87c527697df7bbb817c4b32
Damião Junior <123269234+juninho-Oliveira@users.noreply.github.com>
2026-02-25 21:37:55 -0300

    Add files via upload

---
+++ aula2502/src/aula2502/App1.java
@@ -0,0 +1,11 @@
+package aula2502;
+
+public class App1 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 15;
+		int soma = a+b;
+		
+		System.out.println("Soma >> "+ soma);
+	}
+}
---
+++ aula2502/src/aula2502/ExAula1.java
@@ -0,0 +1,33 @@
+package aula2502;
+
+public class ExAula1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Faça um programa que apresente o total da soma dos
+		 * cem primeiros mnumeros inteiros */
+		
+		/*int soma = 0;
+		int y = 100;
+		
+		for(int i = 1 ; i  <= y; i++ ) {
+			soma = soma+i;
+			System.out.println("Soma : " + soma);
+		}
+			
+		System.out.println("Soma dos 100 primeiros numeros: " + soma);*/
+		
+		/*Faça um aplicativo que calcule o produto dos inteiros 
+		 * impares de 1 a 15 e exiba o resultado na tela */
+		
+		int produto = 1;
+		
+		for(int i = 1 ; i  <= 15; i+=2) {
+			produto = produto*i;
+			System.out.println("Soma : " + produto);
+		}
+		
+		System.out.println("Soma dos 100 primeiros numeros: " + produto);
+	}
+
+}
